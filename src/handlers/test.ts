4dd0ad15172e1f653f5a53f2ff405c10b9905321
rldnd <gi981226@gmail.com>
2024-11-16 22:43:45 +0900

    feat(lint): prettier, eslint, husky

--- src/handlers/test.ts
+++ src/handlers/test.ts
@@ -1,18 +1,13 @@
-import type {
-  Context,
-  APIGatewayProxyStructuredResultV2,
-  APIGatewayProxyEventV2,
-  Handler,
-} from "aws-lambda";
+import type { Context, APIGatewayProxyStructuredResultV2, APIGatewayProxyEventV2, Handler } from 'aws-lambda';
 
 export const handler: Handler = async (
   _event: APIGatewayProxyEventV2,
   _context: Context
 ): Promise<APIGatewayProxyStructuredResultV2> => {
-  console.log("Hello, Lambda 1!");
+  console.log('Hello, Lambda 1!');
 
   return {
     statusCode: 200,
-    body: "hello world",
+    body: 'hello world',
   };
 };
