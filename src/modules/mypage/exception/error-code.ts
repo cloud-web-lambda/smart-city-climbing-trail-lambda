86643a788b9b882041bd3968f9357fc71ce0cd1b
gongPyeon <pyeonkang@naver.com>
2024-12-04 02:44:48 +0900

    refactor: 수정된 에러 메세지 적용

--- src/modules/mypage/exception/error-code.ts
+++ src/modules/mypage/exception/error-code.ts
@@ -4,7 +4,7 @@ import type { ErrorCodeMapper } from '@/common/@types/common';
 export const ERROR_MESSAGE = {
   MISSING_REQUIRED_PARAM: '필수 파라미터가 누락되었습니다.',
   USER_NOT_FOUND: '몸무게 정보를 저장하지 않았습니다.',
-  MISSING_ACCESS_TOKEN: '액세스 토큰이 누락되었습니다.'
+  MISSING_ACCESS_TOKEN: '액세스 토큰이 누락되었습니다.',
 };
 
 export const ERROR_CODE: ErrorCodeMapper<typeof ERROR_MESSAGE> = {
